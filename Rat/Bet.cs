9e632d35f40d401dc0e120ce5beb8512da2359b0
Ben Krüger <benx0071@edu.sde.dk>
2023-11-24 11:28:06 +0100

    Added public referenc atribute to Bet class, for _money atribute.

--- Rat/Bet.cs
+++ Rat/Bet.cs
@@ -4,6 +4,7 @@ namespace DLL
 	public class Bet
 	{
 		private int _money { get; set; }
+		public int Money { get { return _money; } }
 		private Player _player { get; set; }
 		private Race _race { get; set; }
 		private Rat _rat { get; set; }
