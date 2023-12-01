a696a4034a1dbb350e193a448170cfacc9a14c2b
Ben Krüger <benx0071@edu.sde.dk>
2023-12-01 09:24:41 +0100

    Created auto generated data for, if database is empty.

--- BLL/Maneger.cs
+++ BLL/Maneger.cs
@@ -1,5 +1,6 @@
 ﻿using BLL.Repository;
 using DLL;
+using DLL.Modles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +28,19 @@ namespace BLL
         public SaveCenter GetSave()
         {
            SaveCenter data = repo.LoadData();
+            if(data == null)
+            {
+               SaveCenter autodata = new SaveCenter();
 
+                Track Bunker = new Track("Bunker", 20);
+                Rat Timmy = new Rat("Timmy", 2, 1);
+                Rat Lucas = new Rat("Lucas", 3, 0);
+                autodata.Rats.Add(Timmy);
+                autodata.Rats.Add(Lucas);
+                autodata.Tracks.Add(Bunker);
+
+                return autodata;
+            }
             return data;
         }
     }
